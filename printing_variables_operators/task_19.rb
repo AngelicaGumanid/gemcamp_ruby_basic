4a2487437d7e7485abdbdbd0818116f622698911
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-09 16:47:13 +0800

    Printing, Variables and Operator, task 19

---
+++ printing_variables_operators/task_19.rb
@@ -0,0 +1,11 @@
+print "Give any number: "
+input = gets.chomp
+print "Give any number to multiply to the first number: "
+input2 = gets.chomp
+
+integer_value1 = input.to_i
+integer_value2 = input2.to_i
+
+result = integer_value1 * integer_value2
+
+puts "#{integer_value1} multiplied to #{integer_value2}, the result is #{result}"
