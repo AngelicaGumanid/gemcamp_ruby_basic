47d6132fda6d12b61264a59196064724e1bba2d1
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-09 16:22:41 +0800

    Printing, Variables and Operator, task 16

---
+++ printing_variables_operators/task_16.rb
@@ -0,0 +1,5 @@
+name = "Angelica Rose Gumanid"
+hobby = "reading, playing and listening to music"
+favorite_color = "blue and black"
+
+print "My name is #{name}. My hobbies are #{hobby} and my favorite colors are #{favorite_color}."
