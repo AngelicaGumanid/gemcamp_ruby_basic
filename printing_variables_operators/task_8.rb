a891653f50bd4fcab79f2873bd0e36ca9a30571b
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 14:51:31 +0800

    Task 8, birthyear and current year then print age

--- printing_variables_operators/task_8.rb
+++ printing_variables_operators/task_8.rb
@@ -3,4 +3,4 @@ current_year = 2024
 
 age = current_year - birth_year
 
-print "The current year is #{current_year}, my birth year is #{birth_year} and my age is #{age}"
+print "The current year is #{current_year}, my birth year is #{birth_year} and my age is #{age}."
