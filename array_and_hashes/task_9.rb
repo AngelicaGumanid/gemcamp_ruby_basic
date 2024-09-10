69339472d44883b139c46e0ee1537f4d848427d6
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 17:27:36 +0800

    Task 9, removing the first element (.shift).

---
+++ array_and_hashes/task_9.rb
@@ -0,0 +1,7 @@
+# Given an array, remove the first element and print the updated array.
+# ["cat", "dog", "rabbit", "hamster"]
+
+pets = ["cat", "dog", "rabbit", "hamster"]
+pets.shift
+
+print "The updated list of removing the first element in the array is: #{pets}"
