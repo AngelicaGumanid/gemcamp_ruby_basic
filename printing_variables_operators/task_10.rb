1054d1053babf5f9deb3e0c05cde57ce463a4321
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 17:46:41 +0800

    Task 10, subtract 2 numbers.

--- printing_variables_operators/task_10.rb
+++ printing_variables_operators/task_10.rb
@@ -1,5 +1,7 @@
+# Subtract two numbers and store the result. Print the sentence: “The difference is ___.”
+
 num_1 = 45
 num_2 = 07
 difference = num_1 - num_2
 
-print "The difference of #{num_1} and #{num_2} is #{difference}."
+print "The difference is #{difference}."
