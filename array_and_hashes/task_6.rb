50f0fc3c5ab0401106699a581b210b9db20e4a6d
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 16:47:07 +0800

    Task 6, joined and seperated by hypen (.join).

--- array_and_hashes/task_6.rb
+++ array_and_hashes/task_6.rb
@@ -1,3 +1,6 @@
+# You have an array of numbers: . Combine all numbers into a string, separated by hyphens ("-"), and print the result.
+# [100, 200, 300, 400, 500]
+
 numbers = [100, 200, 300, 400, 500]
 result = numbers.join (" - ")
 
