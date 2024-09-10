64aec2146713d5f12c2c493205ba936ff9857567
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 11:42:11 +0800

    Printing, Variables and Operator, task 26

--- printing_variables_operators/task_25.rb
+++ printing_variables_operators/task_25.rb
@@ -2,4 +2,3 @@ temperature_celsius = 25
 temperature_fahrenheit = (temperature_celsius * 9/5) + 32
 
 print "The temperature in Celsius is #{temperature_celsius}°C, which is #{temperature_fahrenheit}°F in Fahrenheit."
-
---
+++ printing_variables_operators/task_26.rb
@@ -0,0 +1,6 @@
+string1 = "Sample"
+string2 = "Test"
+
+combined = string1[0, 3] + string2[-2, 2]
+
+print "The concatenated result is '#{combined}'."
