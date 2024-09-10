156e7c15c74355c0fa454c9cfc0fefe5b9215615
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 17:03:06 +0800

    Task 7, array of 5 favorite movies and length of array.

---
+++ array_and_hashes/task_7.rb
@@ -0,0 +1,7 @@
+# Create an array of your 5 favorite movies. Print the length of the array.
+
+favorite_movies = ["Harry Potter", "Twilight", "Percy Jackson (Disney Series)", "Grand Turismo", "Twister"]
+
+array = favorite_movies.length
+
+puts "My favorite movies are #{favorite_movies} and the length of the array is = #{array}."
