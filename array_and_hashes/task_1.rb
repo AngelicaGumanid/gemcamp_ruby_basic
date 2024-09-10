3dc9d148b7b84ff284c8889cc5ae69c9fde1a22c
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 16:40:46 +0800

    Task 1, add orange to the list.

--- array_and_hashes/task_1.rb
+++ array_and_hashes/task_1.rb
@@ -1,5 +1,7 @@
-fruits = ["apple", "banana", "cherry", "date"]
+# You have a list of fruits, Add the fruit "orange" to the list and print the updated list.
+# ["apple", "banana", "cherry", "date"]
 
+fruits = ["apple", "banana", "cherry", "date"]
 fruits[4] = "orange"
 
 print "Updated list of array of Fruits: #{fruits}"
