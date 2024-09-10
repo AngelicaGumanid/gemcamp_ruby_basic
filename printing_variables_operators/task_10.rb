6639e2654dd6c84ae7033c7b7c4f5351757b5a23
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 14:57:35 +0800

    Task 10, subtract 2 numbers.

--- printing_variables_operators/task_10.rb
+++ printing_variables_operators/task_10.rb
@@ -2,4 +2,4 @@ num_1 = 45
 num_2 = 07
 difference = num_1 - num_2
 
-print "The difference of #{num_1} and #{num_2} is #{difference}."
+print "The difference of #{num_1} and #{num_2} is #{difference}"
