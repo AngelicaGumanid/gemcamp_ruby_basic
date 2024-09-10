cc8f881bc1803828bef700be08fa6598a16b0989
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 16:43:03 +0800

    Task 3, print the third day of the week.

--- array_and_hashes/task_3.rb
+++ array_and_hashes/task_3.rb
@@ -1,3 +1,5 @@
+# Create an array that contains the days of the week, from Monday to Sunday. Print the third day in the array.
+
 days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday" "Friday", "Saturday", "Sunday"]
 
 print "The third day of the week is: #{days_of_the_week[2]}"
