1151d72ce0cd9e451c365a412752dabbcaa4579c
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 18:07:15 +0800

    Task 17, result of mathematical expression.

--- printing_variables_operators/task_17.rb
+++ printing_variables_operators/task_17.rb
@@ -1,3 +1,5 @@
+# Print the result of a mathematical expression: (25 - 5) + (12 * 3).
+
 mathematical_expression = (25 - 5) + (12 * 3)
 
 print "Mathematical Expression: (25 - 5) + (12 * 3) and the result is #{mathematical_expression}."
