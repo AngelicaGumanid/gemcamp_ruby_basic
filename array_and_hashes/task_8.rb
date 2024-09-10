cb7afccdbcbc23ef57db9e4ef3bc5b428ba25d4b
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 17:23:13 +0800

    Task 8, printing the highest score of the given student scores.

---
+++ array_and_hashes/task_8.rb
@@ -0,0 +1,5 @@
+# You have an array of students’ scores, Find the highest score in the array and print it.
+# [85, 90, 78, 92, 88]
+
+students_scores = [85, 90, 78, 92, 88]
+students_scores.each_with_index.max[1]
