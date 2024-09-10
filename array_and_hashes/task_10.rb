bdf1421f692d4940a5186972d939c81aeec5f2c3
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 17:30:55 +0800

    Task 10, print the index of Mexico.

---
+++ array_and_hashes/task_10.rb
@@ -0,0 +1,4 @@
+# You have an array of countries, Find and print the index of "Mexico".
+# ["USA", "Canada", "Mexico", "Germany", "France"]
+
+countries = ["USA", "Canada", "Mexico", "Germany", "France"]
