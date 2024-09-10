f6f4e697ae607c4e26cc8fa3c84bc48ec9c070ad
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 16:44:28 +0800

    Task 4, replacing colors.

--- array_and_hashes/task_4.rb
+++ array_and_hashes/task_4.rb
@@ -1,3 +1,6 @@
+# You have an array of colors, Replace "yellow" with "purple" and print the new array.
+# ["red", "blue", "green", "yellow"]
+
 colors = ["red", "blue", "green", "yellow"]
 colors[3] = "purple"
 
