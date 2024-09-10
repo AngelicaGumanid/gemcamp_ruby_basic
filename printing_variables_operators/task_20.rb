4bc3a0745238a4a5cd8fe9eb2cd8e8d83d9554dc
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-10 16:37:05 +0800

    Task 20, birth year and calculate the age in 5 years.

--- printing_variables_operators/task_20.rb
+++ printing_variables_operators/task_20.rb
@@ -8,5 +8,5 @@ age = current_year - interger_value
 
 five_years_age = age + 5
 
-print "The current year is #{current_year} and your birth year is #{interger_value}. Your current age now is #{age} and in the next 5 years will be #{five_years_age}"
+print "The current year is #{current_year} and your birth year is #{interger_value}. Your current age now is #{age} and in the next 5 years will be #{five_years_age}."
 
