5ee045a73e9c0ad8ca1fad7680b13637cf623aa2
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 15:32:39 +0800

    Task 1, check if the inputted number is positive.

---
+++ conditional_statements/task_1.rb
@@ -0,0 +1,16 @@
+# Write a program that asks the user for a number and checks if it is positive. If the number is positive, print The number is positive.
+
+print "Enter a number: "
+number = gets.chomp
+
+integer_value = number.to_i
+
+if integer_value >= 1
+  puts "The number #{integer_value} is positive."
+elsif integer_value <= -1
+  puts "The number #{integer_value} is negative."
+elsif integer_value == 0
+  puts "The number #{integer_value} is neutral"
+else
+  "Null"
+end
