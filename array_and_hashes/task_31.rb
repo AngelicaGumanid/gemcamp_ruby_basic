23838b0bfde4793c7532f9e3dbd0d84911d56588
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 14:23:41 +0800

    Task 31, hash of hashes two different student.

---
+++ array_and_hashes/task_31.rb
@@ -0,0 +1,5 @@
+# Create a hash of hashes to represent two different students and their grades in Math and English. Print the English grade of the second student
+
+students = { student1: { math: 85, english: 90 }, student2: { math: 78, english: 92 } }
+
+puts "The English grade of the second student is #{students[:student2][:english]}."
