d70650cb39919727458b31e0c173afc2838a2cef
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 18:51:51 +0800

    Task 1, check if the inputted number is positive.

--- conditional_statements/task_1.rb
+++ conditional_statements/task_1.rb
@@ -1,16 +1,14 @@
 # Write a program that asks the user for a number and checks if it is positive. If the number is positive, print The number is positive.
 
 print "Enter a number: "
-number = gets.chomp
+number = gets.to_i
 
-integer_value = number.to_i
-
-if integer_value >= 1
-  puts "The number #{integer_value} is positive."
-elsif integer_value <= -1
-  puts "The number #{integer_value} is not positive."
-elsif integer_value == 0
-  puts "The number #{integer_value} is neutral."
+if number>= 1
+  puts "The number #{number} is positive."
+elsif number <= -1
+  puts "The number #{number} is not positive."
+elsif number == 0
+  puts "The number #{number} is neutral."
 else
   "Null "
 end
---
+++ conditional_statements/task_27.rb
@@ -0,0 +1,2 @@
+# Write a program that asks for a string and checks if it is empty or only contains spaces. If either condition is true, print The string is empty or only contains spaces. Otherwise, print The string has valid content.
+
