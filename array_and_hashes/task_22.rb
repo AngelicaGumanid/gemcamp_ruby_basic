66ecbe55faab6ae3f811454af458b3a5581f10a1
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 13:50:02 +0800

    Task 22, hash of items in shopping cart (.delete).

---
+++ array_and_hashes/task_22.rb
@@ -0,0 +1,8 @@
+# You have a hash of items in a shopping cart, Remove the item "watch" from the cart and print the updated hash.
+# { shoes: 50, bag: 30, watch: 20 }
+
+shopping_cart = { shoes: 50, bag: 30, watch: 20 }
+
+shopping_cart.delete(:watch)
+
+puts "The updated shopping cart hash is #{shopping_cart}."
