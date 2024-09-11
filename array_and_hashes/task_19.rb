aaeaa4cb010545478185d38415470b15b0dc8ffd
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 12:35:40 +0800

    Task 19, hashing representing library book.

---
+++ array_and_hashes/task_19.rb
@@ -0,0 +1,5 @@
+# Create a hash to represent a library book with the following information: title is "koda", author is "mark", and pages is 100. Print the book's title.
+
+book = { title: "koda", author: "mark", pages: 100 }
+
+print "The book's title is '#{book[:title]}'."
