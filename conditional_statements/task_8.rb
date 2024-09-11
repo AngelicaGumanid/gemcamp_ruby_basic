f01f476efff3637d63ba84f591cf4c8acfcc8a22
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 16:31:24 +0800

    Task 8, check if the inputted number is divisible of 5.

---
+++ conditional_statements/task_8.rb
@@ -0,0 +1,13 @@
+# Write a program that asks for a number and checks if it is divisible by 5.
+# If it is, print The number is a multiple of 5.
+
+print "Enter a number: "
+number = gets.chomp
+
+integer_value = number.to_i
+
+if (integer_value % 5) == 0
+  puts "The number is a multiple of 5."
+else
+  puts "The number is not a multiple of 5."
+end
