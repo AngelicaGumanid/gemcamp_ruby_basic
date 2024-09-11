4f021e8178b5241eb80734d0edf0ae2baa413f17
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 16:54:00 +0800

    Task 13, check if the inputted number are equals or not equal.

---
+++ conditional_statements/task_13.rb
@@ -0,0 +1,16 @@
+# Write a program that takes two numbers and checks if they are equal. If they are equal, print The numbers are equal. Otherwise, print The numbers are not equal.
+
+print "Enter a number: "
+number1 = gets.chomp
+
+print "Enter another number: "
+number2 = gets.chomp
+
+integer_value1 = number1.to_i
+integer_value2 = number2.to_i
+
+if integer_value1 == integer_value2
+  puts "The #{integer_value1} and #{integer_value2} numbers are equal."
+else
+  puts "The #{integer_value1} and #{integer_value2} numbers are not equal. "
+end
