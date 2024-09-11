f332d150af63ad16614f47199a83f94608b23a09
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 10:25:40 +0800

    Task 12, reversed array.

---
+++ array_and_hashes/task_12.rb
@@ -0,0 +1,8 @@
+# Given an array of numbers , reverse the array and print the result.
+# [3, 6, 9, 12, 15]
+
+numbers = [3, 6, 9, 12, 15]
+reversed_numbers = numbers.reverse
+
+puts "The original array is #{numbers}"
+puts "The reversed array is #{reversed_numbers}."
