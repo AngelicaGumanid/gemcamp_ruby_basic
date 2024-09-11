c20b247878f6d0267cefddb4b8dd82f9f0582a26
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 14:36:39 +0800

    Task 35, print the second red fruit.

---
+++ array_and_hashes/task_35.rb
@@ -0,0 +1,6 @@
+# Print the second red fruit.
+# { red: ["apple", "cherry"], yellow: ["banana", "lemon"]}
+
+fruits = { red: ["apple", "cherry"], yellow: ["banana", "lemon"]}
+
+print "The second red fruit is #{fruits[:red][1]}."
