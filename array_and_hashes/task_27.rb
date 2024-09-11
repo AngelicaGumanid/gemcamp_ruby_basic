483c83d27c61832c61f328d0cd9edd4c37feafd4
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 14:10:04 +0800

    Task 27, print the values in the hash.

---
+++ array_and_hashes/task_27.rb
@@ -0,0 +1,6 @@
+# Print all the values in the hash.
+# { red: "#FF0000", green: "#00FF00", blue: "#0000FF" }
+
+colors_hash = { red: "#FF0000", green: "#00FF00", blue: "#0000FF" }
+
+print "The values in the hash are #{colors_hash.values}."
