1a4d77751ebc7a50e54de0cba59cd740dd715537
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 17:58:15 +0800

    Task 22, check if the inputted age or a member to have discount.

---
+++ conditional_statements/task_22.rb
@@ -0,0 +1,16 @@
+# Write a program that asks the user for their age and membership status. If the person is a member or they are older than 60, print You are eligible for a discount. Otherwise, print You are not eligible for a discount.
+
+print "Enter your age: "
+age = gets.chomp
+
+integer_value = age.to_i
+
+puts "\nType 'member' if you are a member, 'not a member' if not."
+print"Enter your membership status: "
+membership_status = gets.chomp
+
+if membership_status == "member" || integer_value > 60
+  puts "You are eligible for a discount."
+else
+  puts "You are not eligible for a discount."
+end
