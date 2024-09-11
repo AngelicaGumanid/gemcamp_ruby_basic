e24735901b35d05da6cc940c080b50d5e5a9ceb9
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 10:36:44 +0800

    Task 13, updated array of amimals.

---
+++ array_and_hashes/task_13.rb
@@ -0,0 +1,6 @@
+# Create an array with five different animals. Replace the third animal with "tiger" and print the updated array.
+
+animals = ["dog", "cat", "hamster", "panda", "sloth"]
+animals[2] = "butterfly"
+
+puts "The updated array of animals is #{animals}."
