f7f6ba9d7493d7531d50f52dc9173631d30effe1
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 14:32:32 +0800

    Task 34, print the second number in the array.

---
+++ array_and_hashes/task_34.rb
@@ -0,0 +1,6 @@
+# Print the second number in the array.
+# { numbers: [10, 20, 30, 40]}
+
+numbers = { numbers: [10, 20, 30, 40]}
+
+print "The second number in the array is #{numbers[:numbers][1]}."
