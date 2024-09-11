3f2d8b374774c3d86d80b8f6fea14efdb88b3fb3
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 14:27:04 +0800

    Task 32, print the model of car 1.

---
+++ array_and_hashes/task_32.rb
@@ -0,0 +1,12 @@
+# Print the model of car1
+#
+#
+#
+# {
+#   car1: {make: "Toyota", model: "Corolla"},
+#   car2: {make: "Ford", model: "Mustang"}
+# }
+
+models = { car1: {make: "Toyota", model: "Corolla"}, car2: {make: "Ford", model: "Mustang"}}
+
+print "The model of Car 1 is #{models[:car1][:model]}."
