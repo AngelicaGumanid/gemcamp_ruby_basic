b0e4713b653ba49da0afa14fe77c263356c93d0f
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 11:35:11 +0800

    Task 17, two new random numbers (.unshift).

---
+++ array_and_hashes/task_17.rb
@@ -0,0 +1,6 @@
+# Create an array of five random numbers. Add two more numbers to the beginning of the array and print the updated array.
+
+random_numbers = [7, 4, 11, 2007, 12]
+random_numbers.unshift(1, 5)
+
+print "The updated array with the two new numbers is #{random_numbers}."
