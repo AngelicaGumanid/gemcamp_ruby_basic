74d5c5305823bfd27041ce4784638cba5e9d5456
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 14:21:43 +0800

    Task 30, capital of France.

---
+++ array_and_hashes/task_30.rb
@@ -0,0 +1,12 @@
+# Print the capital of France.
+#
+#
+#
+# {
+#  usa: { capital: "Washington", population: 331},
+#  france: { capital: "Paris", population: 67}
+# }
+
+capital = { usa: { capital: "Washington", population: 331}, france: { capital: "Paris", population: 67}}
+
+print "The capital of France is #{capital[:france][:capital]}."
