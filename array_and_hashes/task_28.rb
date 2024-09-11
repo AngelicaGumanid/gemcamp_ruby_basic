55b189ccf9e8dfcc1df3b662a7c1cf92d2c68347
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 14:14:25 +0800

    Task 28, hash to store info about fav movie.

---
+++ array_and_hashes/task_28.rb
@@ -0,0 +1,5 @@
+# Create a hash to store information about your favorite movie: title, director, year, and rating. Print the entire hash.
+
+favorite_movie = { title: "Harry Potter", director: "J.K. Rowling", year: 2001, rating: 7.6 }
+
+print "Favorite movie details: #{favorite_movie}."
