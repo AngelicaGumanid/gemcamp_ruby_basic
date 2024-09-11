909cbda7c4655825d3dc823e39e85e9649c1378e
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 17:01:06 +0800

    Task 14, check if the inputted word is long or short.

---
+++ conditional_statements/task_15.rb
@@ -0,0 +1,10 @@
+# Write a program that asks for a word and checks if the word has more than 5 characters. If it does, print The word is long. Otherwise, print The word is short.
+
+print "Please enter a word: "
+word = gets.chomp
+
+if word.length >= 5
+  puts "The word is long."
+else
+  puts "The word is short."
+end
