9e30e14d45f29a2fcd510f0f0046396ff2a4a294
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 11:45:53 +0800

    Task 18, remove dumplicate elements (.uniq).

---
+++ array_and_hashes/task_18.rb
@@ -0,0 +1,6 @@
+# Given an array [1, 2, 3, 4, 5], remove any duplicate elements (if there are any) and print the unique values.
+
+numbers = [1, 2, 3, 4, 5, 3, 1]
+unique_values = numbers.uniq
+
+puts "The unique values are #{unique_values}."
