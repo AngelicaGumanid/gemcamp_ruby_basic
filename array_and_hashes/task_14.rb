80fea7d211d42fad3ece0e551343841a9d2fc27e
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 10:46:05 +0800

    Task 14, combined array of colors.

---
+++ array_and_hashes/task_14.rb
@@ -0,0 +1,8 @@
+# You have two arrays: ["red", "green", "blue"] and ["yellow", "purple", "pink"]. Combine them into one array and print the result.
+
+colors1 = ["red", "green", "blue"]
+colors2 = ["yellow", "purple", "pink"]
+
+combined_colors = colors1 + colors2
+
+puts "The combined array is: #{combined_colors}."
