f53052c836a8dbee406432abbdcea7e6a52ba059
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 15:54:30 +0800

    Task 1, check if the inputted number is 18 above to vote.

---
+++ conditional_statements/task_4.rb
@@ -0,0 +1,12 @@
+# Write a program that asks for a person's age and checks if they are 18 or older. If they are, print You are eligible to vote.
+
+print "Enter your age: "
+age = gets.chomp
+
+integer_value = age.to_i
+
+if integer_value >= 18
+  puts "You are eligible to vote."
+else
+  puts "You are not eligible to vote."
+end
