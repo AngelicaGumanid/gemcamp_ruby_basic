620ee4aec78e5c3ef8bfba316d5c8570090bac9c
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-11 10:22:56 +0800

    Task 11, last name in the array.

---
+++ array_and_hashes/task_11.rb
@@ -0,0 +1,6 @@
+# Create an array with the names of 4 family members. Print the last name in the array.
+
+family = ["John", "Alice", "Emma", "David"]
+last_name = family[-1]
+
+puts "The last name in the array is #{last_name}."
