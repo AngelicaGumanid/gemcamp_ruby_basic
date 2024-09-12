080dc6a6a21988214a862755815b5918dbbb6f94
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 17:13:12 +0800

    Task 40, find the largest number.

---
+++ conditional_statements/task_40.rb
@@ -0,0 +1,7 @@
+# Write a program that loops through an array of numbers and finds the largest number. Print the largest number at the end.
+
+numbers = [7, 39, 50, 11, 4, 53, 52]
+
+largest_number = numbers.max
+
+puts "The largest number is: #{largest_number}"
