d80b711cafb04f6fadb09967faf8396aaf07a388
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 10:37:11 +0800

    Task 20, check if the inputted number is divisible of 10.

--- conditional_statements/task_20.rb
+++ conditional_statements/task_20.rb
@@ -1,11 +1,9 @@
 # Write a program that asks for a number and checks if it is divisible by 10. If it is, print The number is a multiple of 10. Otherwise, print The number is not a multiple of 10.
 
 print "Enter a number: "
-number = gets.chomp
+number = gets.chomp.to_i
 
-integer_value = number.to_i
-
-if (integer_value % 10) == 0
+if (number % 10) == 0
   puts "The number is multiple of 10."
 else
   puts "The number is not a multiple of 10"
