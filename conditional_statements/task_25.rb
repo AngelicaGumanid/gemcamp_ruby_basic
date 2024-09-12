4bb398bcaa7bec687bfcac8459a886abab8b9c07
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 10:43:35 +0800

    Task 25, check if divisible by 3 and 5, by 3, by 5 and not.

--- conditional_statements/task_25.rb
+++ conditional_statements/task_25.rb
@@ -5,17 +5,15 @@
 # Otherwise, print the number is not divisible by 3 or 5.
 
 print "Enter a number: "
-number = gets.chomp
+number = gets.chomp.to_i
 
-integer_value = number.to_i
-
-if (integer_value % 3) == 0 && (integer_value % 5) == 0
+if (number % 3) == 0 && (number % 5) == 0
   puts "The number is divisible by both 3 and 5"
 
-elsif (integer_value % 3) == 0
+elsif (number % 3) == 0
   puts "The number is divisible by 3"
 
-elsif (integer_value % 5) == 0
+elsif (number % 5) == 0
     puts "The number is divisible by 5"
 
 else
