1b4edbc9a9e682335be8db0c810259919e401ea7
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 10:26:00 +0800

    Task 10, check if the inputted age is a teenager.

--- conditional_statements/task_10.rb
+++ conditional_statements/task_10.rb
@@ -1,11 +1,9 @@
 # Write a program that asks for a person's age and checks if they are a teenager (between 13 and 19). If they are, print You are a teenager.
 
 print "Enter your age: "
-age = gets.chomp
+age = gets.to_i
 
-integer_value = age.to_i
-
-if integer_value >= 13 && integer_value <= 19
+if age >= 13 && age <= 19
   puts "You are a teenager."
 else
   puts "You are not a teenager"
