0f8160872aa75a4b448b98407244273f3d6315c2
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 13:52:06 +0800

    Task 31, array loop of even numbers.

---
+++ conditional_statements/task_31.rb
@@ -0,0 +1,9 @@
+# Write a program that loops through an array of numbers and prints each number if it is even.
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+array.each do |number|
+  if (number % 2) == 0
+    puts number
+  end
+end
