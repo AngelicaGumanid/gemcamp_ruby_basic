d769a9773516d60bacbc93bd843699f538abc0b9
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 10:42:36 +0800

    Task 24, check age and a guest to enter the club.

--- conditional_statements/task_24.rb
+++ conditional_statements/task_24.rb
@@ -1,15 +1,14 @@
 # Write a program that asks the user for their age and whether they are on the guest list. If they are older than 18 and on the guest list, print You can enter the club. Otherwise, print You cannot enter.
 
 print "Enter your age: "
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "\nIf you are a guest type 'yes', if not type 'no'."
 print "Are you on a guest list? "
 guest = gets.chomp
 
-integer_value = age.to_i
 
-if integer_value >= 18 && guest == "yes"
+if age >= 18 && guest == "yes"
   puts "\nYou can enter the club."
 else
   puts "\nYou cannot enter."
