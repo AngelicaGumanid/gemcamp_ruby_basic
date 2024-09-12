25804ecab1930dcd0073617c42efa25cbbb29b6a
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 16:26:22 +0800

    Task 36, capitalized the array.

--- conditional_statements/task_36.rb
+++ conditional_statements/task_36.rb
@@ -3,9 +3,9 @@
 names = ["ange", "jerome", "jarl", "lloyd", "lyra", "precious", "reiner", "joshua"]
 
 # Create a new array to store the capitalized names
-capitalized_names = names.map do |name|
+capitalized = names.map do |name|
   name.capitalize
 end
 
 # Print the updated array of capitalized names
-puts "Capitalized names: #{capitalized_names.inspect}"
+puts "Capitalized names: #{capitalized.inspect}"
