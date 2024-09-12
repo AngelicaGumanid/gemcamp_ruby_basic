b363ccf44293e27f7a7747c98761610e083d62f2
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 10:44:45 +0800

    Task 26, age is 18 above and drived the driving test.

--- conditional_statements/task_26.rb
+++ conditional_statements/task_26.rb
@@ -1,15 +1,13 @@
 # Write a program that asks for the user’s age and whether they have passed the driving test. If they are at least 18 and have passed the test, print You are eligible for a driver's license. Otherwise, print You are not eligible.
 
 print "Enter your age: "
-age = gets.chomp
-
-integer_value = age.to_i
+age = gets.chomp.to_i
 
 puts "\nType 'yes' if you passed your driving test, 'no' if not."
 print"Did you passed you driving test: "
 test = gets.chomp
 
-if test == "yes" && integer_value >= 18
+if test == "yes" && age >= 18
   puts "\nYou eligible for a driver's license."
 else
   puts "\nYou are not eligible for a driver license."
