aa4d8425bac94ad479cd15ea45bf44e53bb0d27c
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 10:28:36 +0800

    Task 12, check if the inputted number is odd or even number.

--- conditional_statements/task_12.rb
+++ conditional_statements/task_12.rb
@@ -1,12 +1,10 @@
 # Write a program that asks the user for a number and checks if it is even or odd. If it’s even, print The number is even. Otherwise, print The number is odd.
 
 print "Enter a number: "
-number = gets.chomp
+number = gets.to_i
 
-integer_value = number.to_i
-
-if (integer_value % 2) == 0
-  puts "The number #{integer_value} is even."
+if (number % 2) == 0
+  puts "The number #{number} is even."
 else
-  puts "The number #{integer_value} is odd. "
+  puts "The number #{number} is odd. "
 end
