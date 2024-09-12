0176fd5fef7dbb7c3eadd65cf9cdf1e6f902b397
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 10:33:13 +0800

    Task 16, check if the inputted number is in range between 10 and 20.

--- conditional_statements/task_16.rb
+++ conditional_statements/task_16.rb
@@ -1,12 +1,10 @@
 # Write a program that asks the user for a number and checks if it is between 10 and 20 (inclusive). If it is, print The number is within range. Otherwise, print The number is out of range.
 
 print "Enter a number: "
-number = gets.chomp
+number = gets.to_i
 
-integer_value = number.to_i
-
-if integer_value < 20 && integer_value > 10
-  puts "The number #{integer_value} is within the range."
+if number < 20 && number > 10
+  puts "The number #{number} is within the range."
 else
   puts "The number is out of range. "
 end
