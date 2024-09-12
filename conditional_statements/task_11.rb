0285c366982d628bb4b7b6c8ad8182e21da00ba1
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 10:27:09 +0800

    Task 25, check if divisible by 3 and 5, by 3, by 5 and not.

--- conditional_statements/task_11.rb
+++ conditional_statements/task_11.rb
@@ -1,16 +1,14 @@
 # Write a program that asks the user for a number and checks if it is positive or negative. If it is positive, print The number is positive Otherwise, print The number is negative.
 
 print "Enter a number: "
-number = gets.chomp
+number = gets.to_i
 
-integer_value = number.to_i
-
-if integer_value >= 1
-  puts "The number #{integer_value} is positive."
-elsif integer_value <= -1
-  puts "The number #{integer_value} is negative."
-elsif integer_value == 0
-  puts "The number #{integer_value} is neutral."
+if number >= 1
+  puts "The number #{number} is positive."
+elsif number <= -1
+  puts "The number #{number} is negative."
+elsif number == 0
+  puts "The number #{number} is neutral."
 else
   "Null "
 end
