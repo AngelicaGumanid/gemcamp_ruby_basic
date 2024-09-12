1bcc65d7377c4c087420865e9d96bb389b23765a
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 10:34:17 +0800

    Task 18, to check if the inputted age is a semior citizen.

--- conditional_statements/task_18.rb
+++ conditional_statements/task_18.rb
@@ -1,11 +1,9 @@
 # Write a program that asks for a person's age and checks if they are 65 or older. If they are, print You are a senior citizen. Otherwise, print You are not a senior citizen.
 
 print "Enter your age: "
-age = gets.chomp
+age = gets.to_i
 
-integer_value = age.to_i
-
-if integer_value >= 65
+if age >= 65
   puts "You are a senior citizen."
 else
   puts "You are not a senior citizen."
