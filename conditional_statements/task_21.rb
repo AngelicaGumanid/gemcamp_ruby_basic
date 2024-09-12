878a789c06add79a196d835a5acfb239a36ac6e0
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-12 10:38:09 +0800

    Task 21, check if the inputted number is between 50 and 100 and even.

--- conditional_statements/task_21.rb
+++ conditional_statements/task_21.rb
@@ -1,11 +1,9 @@
 # Write a program that asks the user for a number and checks if it is between 50 and 100 (inclusive) and if it is even. Print The number is valid and even. if both conditions are true, otherwise print The number does not meet the criteria.
 
 print "Enter a number: "
-number = gets.chomp
+number = gets.chomp.to_i
 
-integer_value = number.to_i
-
-if integer_value >= 50 && integer_value <= 100 && (integer_value % 2) == 0
+if number >= 50 && number <= 100 && (number % 2) == 0
   puts "The number is valid and even."
 else
   puts "The number does meet the criteria."
