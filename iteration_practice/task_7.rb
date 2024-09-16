2e9327e4f2113a976b0530170121e3686a2a7e72
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-16 10:55:34 +0800

    Iterate and use times method to print the word.

---
+++ iteration_practice/task_7.rb
@@ -0,0 +1,9 @@
+# Create a program that asks the user for two numbers: how many times to iterate and a word to print. Use the times method to print the word that many times, with each word on a new line.
+
+puts "How many times to iterate?"
+iterations = gets.to_i
+
+puts "Enter a word:"
+
+word = gets.chomp
+iterations.times { puts word }
