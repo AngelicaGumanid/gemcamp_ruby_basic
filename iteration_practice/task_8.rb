76deaeaa5400f8ec443412a65ca653ab6d3ab920
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-16 11:13:53 +0800

    Task 8, Use time method to reverse array of string.

--- iteration_practice/task_8.rb
+++ iteration_practice/task_8.rb
@@ -1,4 +1,4 @@
 # Use the times method to reverse an array of strings. Print each string in reverse order of its original position.
 
-arr = ["apple", "banana", "cherry"]
+arr = ["apple", "banana", "cherry" ]
 arr.reverse.each { |str| puts str }
